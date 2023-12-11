c6b7233f15de83bf69ab38982137f3ccdaf4aaa7
Sam Richmond <sam.richmond@cinch.co.uk>
2023-12-11 09:21:34 +0000

    2023/day3

--- 2023/challenge 3/partOne.js
+++ 2023/challenge 3/partOne.js
@@ -1,2 +1,34 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().split("\n");
+
+const testData = [
+  // `48.................501....33.....622..............763.........331.................161.683......................................980..........`,
+  `...491.842.....948*..................338.....*......=...........-...309.......633*....*....................*990...706...452......*..+.......`,
+  //`...*...*....................426........*..408.........................*............659...............250.........&.......*.....767...403....`,
+];
+let total = 0;
+
+testData.forEach((line) => {
+  console.log({ line });
+
+  const regex = /\d+(?=[^\w\s.])|(?<=[^\w\s.])\d+/gm;
+  const isUsableNumber = line.match(regex);
+
+  isUsableNumber.forEach((usableNumber) => {
+    const position = line.indexOf(usableNumber);
+    const length = usableNumber.length;
+    console.log({ usableNumber, position, length });
+  });
+
+  if (isUsableNumber) {
+    isUsableNumber.forEach((number) => (total += Number(number)));
+  }
+});
+
+console.log({ total });
+
+/*
+..***..   
+..*4*..
+..***..
+*/
